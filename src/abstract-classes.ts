30e23d8f119f23dfa4c13030ba662b8b585a3e45
yancharyu <s.ryu1135@gmail.com>
2022-06-26 14:23:39 +0900

    first commit

---
+++ src/abstract-classes.ts
@@ -0,0 +1,18 @@
+export {};
+
+abstract class Animal {
+  abstract cry(): string;
+}
+
+class Lion extends Animal {
+  cry() {
+    return 'roar';
+  }
+}
+
+class Tiger extends Animal {
+  cry() {
+    return 'grrrr';
+  }
+
+}
